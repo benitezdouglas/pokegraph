dd7f1945e2fc616945c09d3edf0a34780a2f45a2
Douglas <douglas.benitez@osf.digital>
2021-03-19 16:08:28 -0300

    fix: path to csv file

--- src/utils/csvjson.parser.ts
+++ src/utils/csvjson.parser.ts
@@ -2,8 +2,7 @@ import { join } from 'path';
 
 import * as csv from "csvtojson";
 
-const csvFilePath = join(__dirname, '../../', 'data', 'csv');
-
 export const csvToJson = async (file) => {
-    return csv().fromFile(`${csvFilePath}\\${file}`);
+    const csvFilePath = join(__dirname, '../../', 'data', 'csv', file);
+    return csv().fromFile(`${csvFilePath}`);
 }
