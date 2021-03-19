b2b9bf632c18c6c68dec6410f5e06b4052bbf071
Douglas <douglas.benitez@osf.digital>
2021-03-19 00:26:37 -0300

    refactor: csvToJson

--- src/utils/csvjson.parser.ts
+++ src/utils/csvjson.parser.ts
@@ -5,7 +5,5 @@ import * as csv from "csvtojson";
 const csvFilePath = join(__dirname, '../../', 'data', 'csv');
 
 export const csvToJson = async (file) => {
-    const jsonArray = await csv().fromFile(`${csvFilePath}\\${file}`);
-
-    console.log(jsonArray);
+    return csv().fromFile(`${csvFilePath}\\${file}`);
 }
